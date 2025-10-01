39122f0b0f63fd1ccb0c96d57f85f381e8faae76
devsharmatech <devp7536@gmail.com>
2025-10-01 11:19:20 +0530

    commit 1Oct25  11 AM

--- src/components/HomePageComponents/Hero.tsx
+++ src/components/HomePageComponents/Hero.tsx
@@ -33,54 +33,101 @@ export default function Hero() {
 
   return (
     <div className="px-5 w-full">
-      <div className="mt-10 flex justify-center w-full px-2">
-        <div className="flex w-full max-w-3xl bg-white rounded-full shadow-xl overflow-hidden border border-gray-200">
-          {/* Search Icon */}
-          <div className="flex items-center pl-4 text-gray-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+      <section
+        className="text-white flex flex-col items-center justify-center px-4 py-12 pb-24 w-full 
+    bg-gradient-to-r from-blue-600 via-blue-500 to-blue-700 rounded-2xl"
+      >
+        <div className="max-w-4xl text-center mx-auto">
+          {/* Heading */}
+          <h1 className="text-[28px] sm:text-[32px] md:text-4xl lg:text-5xl font-bold leading-snug text-white">
+            Expert Solutions for{" "}
+            <span className="text-gray-300">Modern Business</span> Challenges
+          </h1>
+
+          <h4 className="text-[18px] sm:text-[20px] md:text-2xl lg:text-3xl mt-4 text-gray-100">
+            Simplify Compliance{" "}
+            <span className="font-semibold text-gray-200">
+              Focus on Growth.
+            </span>
+          </h4>
+
+          {/* Buttons */}
+          <div className="flex flex-wrap justify-center gap-4 pt-6 w-full">
+            <button
+              className="px-5 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 w-full sm:w-auto text-base sm:text-lg md:text-xl 
+          font-bold text-gray-700 bg-white rounded-2xl shadow-lg border border-gray-300
+          hover:bg-gray-100 hover:scale-105 transition-all duration-300 ease-in-out"
+              onClick={() => setIsOpenEnquiry((pre) => !pre)}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-4.35-4.35M11 19a8 8 0 100-16 8 8 0 000 16z"
-              />
-            </svg>
+              Enquiry Now
+            </button>
+
+            <Link href="/services">
+              <button
+                className="px-5 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 w-full sm:w-auto text-base sm:text-lg md:text-xl 
+            font-bold text-white bg-gradient-to-r from-black via-black to-black
+            rounded-2xl shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out"
+              >
+                {heroContent?.ctaSecondaryLabel || "Explore Our Services"}
+              </button>
+            </Link>
           </div>
 
-          {/* Input */}
-          <input
-            type="text"
-            placeholder="Search for services, solutions..."
-            className="flex-grow 
-        px-3 py-2 sm:px-4 sm:py-3 md:px-5 md:py-4
-        text-sm sm:text-base md:text-lg lg:text-xl
-        text-gray-700 outline-none
-        min-w-0
-        transition"
-          />
+          <div className="mt-10 flex justify-center w-full ">
+            <div className="flex w-full max-w-3xl bg-white rounded-full shadow-xl overflow-hidden border border-gray-200">
+              {/* Search Icon */}
+              <div className="flex items-center pl-4 text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-4.35-4.35M11 19a8 8 0 100-16 8 8 0 000 16z"
+                  />
+                </svg>
+              </div>
 
-          {/* Button */}
-          <button
-            className="flex-shrink-0
-        px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 lg:px-10 lg:py-5
-        bg-gradient-to-r from-black to-black
-        text-white font-semibold
-        text-sm sm:text-base md:text-lg lg:text-xl
-        hover:from-gray-700 hover:to-gray-900
-        rounded-full
-        transition-all duration-300"
-          >
-            Search
-          </button>
+              {/* Input */}
+              <input
+                type="text"
+                placeholder="Search for services, solutions..."
+                className="flex-grow 
+               px-3 py-2 sm:px-4 sm:py-3 md:px-5 md:py-4
+               text-lg sm:text-base md:text-lg lg:text-xl
+               text-gray-700 outline-none
+               rounded-lg
+               transition"
+              />
+
+              {/* Button */}
+              <button
+                className="px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 lg:px-10 lg:py-5
+               bg-gradient-to-r from-black to-black
+               text-white font-semibold
+               text-lg sm:text-base md:text-lg lg:text-xl
+               hover:from-gray-700 hover:to-gray-900
+               rounded-full
+               transition-all duration-300"
+              >
+                Search
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
 
+        {/* Enquiry Modal */}
+        {isOpenEnquiry && (
+          <Modal isOpen={isOpenEnquiry} onClose={() => setIsOpenEnquiry(false)}>
+            <ContactForm />
+          </Modal>
+        )}
+      </section>
 
       {/* Services Section */}
       <div className="services-home mt-8">
